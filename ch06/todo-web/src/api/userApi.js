4d5f88f78e44c3149756161707d716482640abcc
JaviJinble <bubaragi23@gmail.com>
2026-06-16 17:51:42 +0900

    수정

--- ch06/todo-web/src/api/userApi.js
+++ ch06/todo-web/src/api/userApi.js
@@ -1,7 +1,13 @@
 import { axiosInstance } from "./axiosInstance";
 
 export const getMeRequest = async () => {
-    const response = await axiosInstance.get("/api/users/me");
+    try {
+        
+            const response = await axiosInstance.get("/api/users/me");
+        
+            return response.data;
 
-    return response.data;
+    } catch(error) {
+        return error.response.data;
+    }
 };
